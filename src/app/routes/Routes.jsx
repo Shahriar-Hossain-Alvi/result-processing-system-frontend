cffc4baca07c0339a815678232af5d314a2004fc
Shahriar-Hossain-Alvi <shahriarhossainalvi@gmail.com>
2026-02-14 20:49:33 +0600

    renamed project name in package.json file

--- src/app/routes/Routes.jsx
+++ src/app/routes/Routes.jsx
@@ -1,7 +1,6 @@
 import { createBrowserRouter, createRoutesFromElements, Route } from "react-router-dom";
 import MainLayout from "../layout/MainLayout.jsx";
 import ErrorPage from "../../pages/public/ErrorPage.jsx";
-import Signin from "../../pages/auth/Signin.jsx";
 import Notice from "../../pages/public/Notice.jsx";
 import Contact from "../../pages/public/Contact.jsx";
 import Faculties from "../../pages/public/Faculties.jsx";
@@ -10,6 +9,7 @@ import TeacherRoutes from "./TeacherRoutes.jsx";
 import StudentRoutes from "./StudentRoutes.jsx";
 import Unauthorize from "../../pages/public/Unauthorize.jsx";
 import RoleBasedRedirects from "../../components/RouteHandlers/RoleBasedRedirects.jsx";
+import Signin from "../../pages/auth/Signin.jsx";
 
 
 const router = createBrowserRouter(
